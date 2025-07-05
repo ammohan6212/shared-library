383ed90a64471760430c6bc6711e1a44300502af
A.Mohan <mohan6121@email.com>
2025-07-05 12:19:27 +0530

    mohan

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,9 +1,8 @@
+// vars/buildDockerImage.groovy
 def call(String imageName, String version, String dockerfileDir = '.') {
     script {
-        def fullTag = "${imageName}:${version}"
-        echo "🚀 Building Docker image ${fullTag} from ${dockerfileDir}..."
-        def dockerImage = docker.build(fullTag, dockerfileDir)
-        echo "✅ Docker image ${fullTag} built successfully."
-        return dockerImage
+        echo "Building Docker image ${imageName}:${version} from ${dockerfileDir}..."
+        sh "docker build -t ${imageName}:${version} ${dockerfileDir}"
+        echo "✅ Docker image built."
     }
 }
--- vars/pushDockerImageToRegistry.groovy
+++ vars/pushDockerImageToRegistry.groovy
@@ -1,9 +1,21 @@
-def call(String registryUrl, String credentialsId, def dockerImage) {
+def call(String registryUrl, String usernameCredentialId, String passwordCredentialId, def dockerImage) {
     script {
-        echo "Pushing Docker image ${dockerImage.imageName()} to ${registryUrl}..."
-        docker.withRegistry(registryUrl, credentialsId) {
-            dockerImage.push()
+        echo "Preparing to push Docker image ${dockerImage} to ${registryUrl}..."
+
+        withCredentials([
+            string(credentialsId: usernameCredentialId, variable: 'DOCKER_USERNAME'),
+            string(credentialsId: passwordCredentialId, variable: 'DOCKER_PASSWORD')
+        ]) {
+            // Perform docker login manually
+            sh "echo $DOCKER_PASSWORD | docker login  -u $DOCKER_USERNAME --password-stdin"
+
+            // Push the image
+            sh "docker push ${dockerImage}"
+
+            // Optional: logout after push
+            sh "docker logout ${registryUrl}"
+
+            echo "✅ Docker image pushed to ${registryUrl}."
         }
-        echo "✅ Docker image pushed to registry."
     }
 }
