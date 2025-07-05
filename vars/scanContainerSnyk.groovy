11abf21eb5e273c525278ec0c8a2c99906c656d1
A.Mohan <mohan6121@email.com>
2025-07-05 11:04:07 +0530

    mohan

--- vars/scanContainerSnyk.groovy
+++ vars/scanContainerSnyk.groovy
@@ -2,12 +2,7 @@
 // Requires SNYK_TOKEN to be set as a secret in Jenkins or environment
 def call(String dockerImageTag, String dockerfilePath = 'Dockerfile') {
     sh "snyk auth 9d262b22-1f2c-4069-adb9-696793789926"
-    sh "snyk container test ${dockerImageTag}"
-    sh "snyk container test ${dockerImageTag} --file=Dockerfile"
-    sh "snyk container test ${dockerImageTag} --exclude-base-image-vulns"
-    sh "snyk container test ${dockerImageTag} --json > report.json"
-    sh "snyk container test ${dockerImageTag} --sarif > report.sarif"
-    sh "snyk container test ${dockerImageTag} --json-file-output=output.json"
+    sh "snyk container test ${dockerImageTag} --file=Dockerfile --json-file-output=snyk-report.json --sarif-file-output=snyk-report.sarif"
     echo "docker scan is completed please check the logs for details"
 
 }
