f3601787a17fde6497f6d272eb187dbf553d6473
Mohan <mohan14242@gmail.com>
2025-07-08 15:55:35 +0530

    mohan

--- vars/scanContainerSnyk.groovy
+++ vars/scanContainerSnyk.groovy
@@ -1,7 +1,7 @@
 def call(String dockerImageTag, String dockerfilePath = 'Dockerfile') {
     withCredentials([string(credentialsId: 'SNYK_API_TOKEN', variable: 'SNYK_TOKEN')]) {
         sh 'snyk auth $SNYK_TOKEN'
-        sh "snyk container test ${dockerImageTag} --file=${dockerfilePath} --exclude-base-image-vulns --severity-threshold=high --json-file-output=snyk-report.json --sarif-file-output=snyk-report.sarif"
+        sh "snyk container test ${dockerImageTag} --file=${dockerfilePath} --exclude-base-image-vulns --severity-threshold=high --json-file-output=snyk-report.json --sarif-file-output=snyk-report.sarif || true"
         echo "Docker scan is completed — please check snyk-report.json and snyk-report.sarif for details."
     }
 }
